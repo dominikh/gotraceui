312d98d4619b862c22880515ba2a5387040adc56
Dominik Honnef <dominik@honnef.co>
2022-10-10 02:16:23 +0200

    layout: add PixelInset

--- layout/layout.go
+++ layout/layout.go
@@ -4,6 +4,7 @@ import (
 	"image"
 
 	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/x/outlay"
 )
 
@@ -58,4 +59,43 @@ func (sg SmallGrid) Layout(gtx layout.Context, rows, cols int, sizeEstimator out
 		return dims
 	}
 	return sg.Grid.Layout(gtx, rows, cols, dimmer, wrapper)
+}
+
+// PixelInset is like Inset, but using pixel coordinates instead of dp.
+type PixelInset struct {
+	Top, Bottom, Left, Right int
+}
+
+func (in PixelInset) Layout(gtx layout.Context, w layout.Widget) layout.Dimensions {
+	top := (in.Top)
+	right := (in.Right)
+	bottom := (in.Bottom)
+	left := (in.Left)
+	mcs := gtx.Constraints
+	mcs.Max.X -= left + right
+	if mcs.Max.X < 0 {
+		left = 0
+		right = 0
+		mcs.Max.X = 0
+	}
+	if mcs.Min.X > mcs.Max.X {
+		mcs.Min.X = mcs.Max.X
+	}
+	mcs.Max.Y -= top + bottom
+	if mcs.Max.Y < 0 {
+		bottom = 0
+		top = 0
+		mcs.Max.Y = 0
+	}
+	if mcs.Min.Y > mcs.Max.Y {
+		mcs.Min.Y = mcs.Max.Y
+	}
+	gtx.Constraints = mcs
+	trans := op.Offset(image.Pt(left, top)).Push(gtx.Ops)
+	dims := w(gtx)
+	trans.Pop()
+	return layout.Dimensions{
+		Size:     dims.Size.Add(image.Point{X: right + left, Y: top + bottom}),
+		Baseline: dims.Baseline + bottom,
+	}
 }
