dominikh/gotraceui

commit d59778fa5d3aa729d476716c49c2119f4980b5dc

Track span start time explicitly
In the future we will want to have spans that aren't backed by events,
and for that we need a different way of storing the span's start time.

cmd/gotraceui/activity.go
@@ -363,7 +363,6 @@ type renderedSpansIterator struct {
 func (it *renderedSpansIterator) next(gtx layout.Context) (spansOut MergedSpans, startPx, endPx float32, ok bool) {
 	offset := it.offset
 	spans := it.spans
-	tr := it.tl.trace
 
 	if offset >= len(spans) {
 		return nil, 0, 0, false
@@ -377,7 +376,7 @@ func (it *renderedSpansIterator) next(gtx layout.Context) (spansOut MergedSpans,
 	s := &spans[offset]
 	offset++
 
-	start := tr.Event(s.event()).Ts
+	start := s.start
 	end := s.end
 	if it.prevEnd > start {
 		// The previous span was extended and grew into this span. This shifts our start position to the right.
@@ -403,7 +402,7 @@ func (it *renderedSpansIterator) next(gtx layout.Context) (spansOut MergedSpans,
 			nextSpan := &spans[offset]
 			// Assume that we stop at this span. Compute the final size and extension. Use that to see
 			// if the next span would be large enough to stand on its own. If so, actually do stop at this span.
-			nextStart := tr.Event(nextSpan.event()).Ts
+			nextStart := nextSpan.start
 			nextEnd := nextSpan.end
 			if adjustedEnd > nextStart {
 				// The current span would have to grow into the next span, making it smaller
@@ -421,7 +420,7 @@ func (it *renderedSpansIterator) next(gtx layout.Context) (spansOut MergedSpans,
 			nextSpan := &spans[offset]
 			// Check if the next gap or span would be large enough to stand on its own. If so, stop merging at the
 			// current span.
-			nextStart := tr.Event(nextSpan.event()).Ts
+			nextStart := nextSpan.start
 			nextEnd := nextSpan.end
 			if time.Duration(nextEnd-nextStart) >= minSpanWidthD || time.Duration(nextStart-end) >= minSpanWidthD {
 				// Don't merge spans or gaps that can stand on their own

cmd/gotraceui/statistics.go
@@ -10,10 +10,9 @@ func computeProcessorBusy(tr *Trace, p *Processor, bucketSize time.Duration) []i
 	total := tr.Events[len(tr.Events)-1].Ts
 	buckets := make([]time.Duration, int(math.Ceil(float64(total)/float64(bucketSize))))
 	for _, span := range p.spans {
-		start := tr.Events[span.event()].Ts
-		d := time.Duration(span.end - start)
-		bucket := time.Duration(start) / bucketSize
-		bucketRemainder := bucketSize - (time.Duration(start) % bucketSize)
+		d := time.Duration(span.end - span.start)
+		bucket := time.Duration(span.start) / bucketSize
+		bucketRemainder := bucketSize - (time.Duration(span.start) % bucketSize)
 
 		for d > bucketRemainder {
 			buckets[bucket] += bucketRemainder

cmd/gotraceui/timeline.go
@@ -353,7 +353,6 @@ func (tl *Timeline) zoom(gtx layout.Context, ticks float32, at f32.Point) {
 }
 
 func (tl *Timeline) visibleSpans(spans Spans) Spans {
-	tr := tl.trace
 	// Visible spans have to end after tl.Start and begin before tl.End
 	start := sort.Search(len(spans), func(i int) bool {
 		s := spans[i]
@@ -364,7 +363,7 @@ func (tl *Timeline) visibleSpans(spans Spans) Spans {
 	}
 	end := sort.Search(len(spans), func(i int) bool {
 		s := spans[i]
-		return tr.Event(s.event()).Ts >= tl.end
+		return s.start >= tl.end
 	})
 
 	return spans[start:end]
@@ -669,7 +668,7 @@ func (tl *Timeline) Layout(gtx layout.Context) layout.Dimensions {
 
 		drawRegionOverlays := func(spans Spans, c color.NRGBA, height int) {
 			for _, s := range tl.visibleSpans(spans) {
-				start := tl.trace.Events[s.event()].Ts
+				start := s.start
 				end := s.end
 
 				if start < tl.start {

cmd/gotraceui/trace.go
@@ -176,7 +176,7 @@ func (t *Trace) Event(ev EventID) *trace.Event {
 
 //gcassert:inline
 func (t *Trace) Duration(s *Span) time.Duration {
-	return time.Duration(s.end - t.Event(s.event()).Ts)
+	return time.Duration(s.end - s.start)
 }
 
 func (t *Trace) Task(id uint64) *Task {
@@ -233,7 +233,7 @@ func (ms MergedSpans) Duration(tr *Trace) time.Duration          { return Spans(
 func (ms MergedSpans) Events(all []EventID, tr *Trace) []EventID { return Spans(ms).Events(all, tr) }
 
 func (spans Spans) Start(tr *Trace) trace.Timestamp {
-	return tr.Events[spans[0].event()].Ts
+	return spans[0].start
 }
 
 func (spans Spans) End() trace.Timestamp {
@@ -266,14 +266,13 @@ func (spans Spans) Events(all []EventID, tr *Trace) []EventID {
 
 type Span struct {
 	// The Span type is carefully laid out to optimize its size and to avoid pointers, the latter so that the garbage
-	// collector won't have to scan any memory of our millions of events. It is currently 24 bytes large, with no padding.
+	// collector won't have to scan any memory of our millions of events. It is currently 32 bytes large, with no padding.
 	//
 	// Instead of pointers, fields like pc and event_ are indices into slices. event_ is a uint40, allowing for a total
 	// of 1 trillion events, or 64 TiB worth of events. This size was chosen to eliminate padding.
 
-	// We track the end time, instead of looking at the next span's start time, because per-P timelines can have gaps,
-	// and filling those gaps would probably use more memory than tracking the end time.
-	end trace.Timestamp
+	start trace.Timestamp
+	end   trace.Timestamp
 	// Stack frame this span represents, for sample tracks
 	pc     uint64
 	event_ [5]byte
@@ -303,8 +302,7 @@ func (s *Span) Events(all []EventID, tr *Trace) []EventID {
 		return tr.Event(ev).Ts >= s.end
 	})
 
-	sTs := tr.Event(s.event()).Ts
-
+	sTs := s.start
 	start := sort.Search(len(all[:end]), func(i int) bool {
 		ev := all[i]
 		return tr.Event(ev).Ts >= sTs
@@ -759,11 +757,11 @@ func loadTrace(path string, mwin *MainWindow) (*Trace, error) {
 			state = stateActive
 
 		case trace.EvGCStart:
-			gc = append(gc, Span{state: stateActive, event_: packEventID(EventID(evID))})
+			gc = append(gc, Span{start: ev.Ts, state: stateActive, event_: packEventID(EventID(evID))})
 			continue
 
 		case trace.EvGCSTWStart:
-			stw = append(stw, Span{state: stateActive, event_: packEventID(EventID(evID))})
+			stw = append(stw, Span{start: ev.Ts, state: stateActive, event_: packEventID(EventID(evID))})
 			continue
 
 		case trace.EvGCDone:
@@ -813,6 +811,7 @@ func loadTrace(path string, mwin *MainWindow) (*Trace, error) {
 					end = -1
 				}
 				s := Span{
+					start:  ev.Ts,
 					event_: packEventID(EventID(evID)),
 					state:  stateUserRegion,
 					end:    end,
@@ -869,7 +868,7 @@ func loadTrace(path string, mwin *MainWindow) (*Trace, error) {
 			}
 		}
 
-		s := Span{state: state, event_: packEventID(EventID(evID))}
+		s := Span{start: ev.Ts, state: state, event_: packEventID(EventID(evID))}
 		if ev.Type == trace.EvGoSysBlock {
 			if debug && res.Events[s.event()].StkID != 0 {
 				panic("expected zero stack ID")
@@ -882,7 +881,7 @@ func loadTrace(path string, mwin *MainWindow) (*Trace, error) {
 		switch pState {
 		case pRunG:
 			p := getP(ev.P)
-			p.spans = append(p.spans, Span{state: stateRunningG, event_: packEventID(EventID(evID))})
+			p.spans = append(p.spans, Span{start: ev.Ts, state: stateRunningG, event_: packEventID(EventID(evID))})
 		case pStopG:
 			// XXX guard against malformed traces
 			p := getP(ev.P)
@@ -899,7 +898,7 @@ func loadTrace(path string, mwin *MainWindow) (*Trace, error) {
 		go func() {
 			for i, s := range g.spans {
 				if i != len(g.spans)-1 {
-					s.end = res.Events[g.spans[i+1].event()].Ts
+					s.end = g.spans[i+1].start
 				}
 
 				stack := res.Stacks[res.Events[s.event()].StkID]
@@ -937,7 +936,7 @@ func loadTrace(path string, mwin *MainWindow) (*Trace, error) {
 							// OPT(dh): use binary search
 							for _, parent := range g.userRegions[depth-1] {
 								// The first parent user region that ends after our region starts has to be our parent.
-								if parent.end >= res.Events[last.event()].Ts {
+								if parent.end >= last.start {
 									last.end = parent.end
 									break
 								}

commit 40fc939abeff0a6051c42d93d8efa4c165fdfd5e

Set correct start times for sample spans
Fixes a regression introduced in
d59778fa5d3aa729d476716c49c2119f4980b5dc.

cmd/gotraceui/activity.go
@@ -1450,6 +1450,7 @@ func addSampleTracks(aw *ActivityWidget, g *Goroutine) {
 				} else {
 					// This is a new span
 					span := Span{
+						start:  ev.Ts,
 						end:    end,
 						pc:     stk[len(stk)-i-1],
 						event_: packEventID(evID),
@@ -1462,6 +1463,7 @@ func addSampleTracks(aw *ActivityWidget, g *Goroutine) {
 			} else {
 				// This is the first span
 				span := Span{
+					start:  ev.Ts,
 					end:    end,
 					pc:     stk[len(stk)-i-1],
 					event_: packEventID(evID),