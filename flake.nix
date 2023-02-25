821db1dc5543a63c513c30bfff1232fab09418e6
Dominik Honnef <dominik@honnef.co>
2023-02-25 03:34:26 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          packages.gotraceui = pkgs.buildGoModule {
+            name = "gotraceui";
+            src = self;
+            vendorSha256 = "sha256-bVCj4ZefRYaWTpPHCHWZtYREHwGHVUrnNQRTbPjobQg=";
+
+            subPackages = ["cmd/gotraceui"];
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = with pkgs;
+              (if stdenv.isLinux then [
+                vulkan-headers
+                libxkbcommon
+                wayland
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXfixes
+                libGL
+              ] else if stdenv.isDarwin then [
+                darwin.apple_sdk_11_0.frameworks.Foundation
+                darwin.apple_sdk_11_0.frameworks.Metal
+                darwin.apple_sdk_11_0.frameworks.QuartzCore
+                darwin.apple_sdk_11_0.frameworks.AppKit
+                darwin.apple_sdk_11_0.MacOSX-SDK
+              ] else [ ]);
+          };
+
+          packages.default = self.packages.${system}.gotraceui;
+
+          apps.gotraceui = flake-utils.lib.mkApp { drv = self.packages.${system}.gotraceui; };
+          apps.default = self.apps.${system}.gotraceui;
+
+          devShells.gotraceui = pkgs.mkShell {
+            inputsFrom = builtins.attrValues self.packages.${system};
+          };
+
+          devShells.default = self.devShells.${system}.gotraceui;
+        }
+    );
+}
