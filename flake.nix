354aa883ae3de90aec4b6fbfeb5af2f324d165ca
Dominik Honnef <dominik@honnef.co>
2023-03-29 00:39:36 +0200

    Teach flake how to build the manual

--- flake.nix
+++ flake.nix
@@ -7,6 +7,28 @@
     flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-small
+
+            adjustbox
+            biber
+            biblatex
+            cleveref
+            csquotes
+            fontsetup
+            hyperxmp
+            latexmk
+            luacode
+            lualatex-math
+            menukeys
+            newcomputermodern
+            relsize
+            siunitx
+            soul
+            xstring
+            ;
+        };
       in
         {
           packages.gotraceui = pkgs.buildGoModule {
@@ -46,6 +68,24 @@
               license = licenses.mit;
               platforms = platforms.all;
             };
+          };
+
+          packages.manual = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "manual";
+            src = self;
+            buildInputs = [ tex ];
+
+            buildPhase = ''
+              mkdir -p .cache/texmf-var
+              export TEXMFHOME=.cache
+              export TEXMFVAR=.cache/texmf-var
+              latexmk -cd -f -pdf -lualatex -interaction=nonstopmode -bibtex-cond1 doc/manual/manual.tex
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp doc/manual/manual.pdf $out/gotraceui.pdf
+            '';
           };
 
           packages.default = self.packages.${system}.gotraceui;
