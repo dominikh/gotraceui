8920a361b182593f4baffb23be5263c6b7140808
Dominik Honnef <dominik@honnef.co>
2023-07-29 19:55:54 +0200

    Use multiple sync.Pool instead of our custom cache implementation
    This has lower lock contention.

--- cmd/gotraceui/cache.go
+++ cmd/gotraceui/cache.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"math/bits"
 	"sync"
 
 	"honnef.co/go/gotraceui/trace/ptrace"
 )
 
 var (
-	uint64SliceCache        = &ConcurrentSliceCache[uint64, []uint64]{}
-	boolSliceCache          = &ConcurrentSliceCache[bool, []bool]{}
-	spanSliceCache          = &ConcurrentSliceCache[ptrace.Span, []ptrace.Span]{}
-	stackSpanMetaSliceCache = &ConcurrentSliceCache[stackSpanMeta, []stackSpanMeta]{}
+	uint64SliceCache        = NewConcurrentSliceCache[uint64, []uint64]()
+	boolSliceCache          = NewConcurrentSliceCache[bool, []bool]()
+	spanSliceCache          = NewConcurrentSliceCache[ptrace.Span, []ptrace.Span]()
+	stackSpanMetaSliceCache = NewConcurrentSliceCache[stackSpanMeta, []stackSpanMeta]()
 )
 
 type Cache[T any] struct {
@@ -66,25 +67,95 @@ func (c *SliceCache[E, T]) Clear() {
 	c.items = c.items[:0]
 }
 
+// Mapping from log2 to size that is to be used
+//
+// TODO(dh): remove some of the steps
+var poolSizes = [...]int{
+	0:  1,
+	1:  2,
+	2:  4,
+	3:  8,
+	4:  16,
+	5:  32,
+	6:  64,
+	7:  128,
+	8:  256,
+	9:  512,
+	10: 1024,
+	11: 2048,
+	12: 4096,
+	13: 8192,
+	14: 16384,
+	15: 32768,
+	16: 65536,
+	17: 131072,
+	18: 262144,
+	19: 524288,
+	20: 1048576,
+	21: 2097152,
+	22: 4194304,
+	23: 8388608,
+	24: 16777216,
+	25: 33554432,
+	26: 67108864,
+}
+
 type ConcurrentSliceCache[E any, T ~[]E] struct {
-	mu    sync.Mutex
-	cache SliceCache[E, T]
+	pools [len(poolSizes)]*sync.Pool
+}
+
+func NewConcurrentSliceCache[E any, T ~[]E]() *ConcurrentSliceCache[E, T] {
+	var pools [len(poolSizes)]*sync.Pool
+	for i, v := range poolSizes {
+		i := i
+		v := v
+		if i == 0 {
+			pools[i] = &sync.Pool{
+				New: func() any {
+					return make(T, 0, 1)
+				},
+			}
+			continue
+		}
+
+		if v == poolSizes[i-1] {
+			pools[i] = pools[i-1]
+		} else {
+			pools[i] = &sync.Pool{
+				New: func() any {
+					return make(T, 0, v)
+				},
+			}
+		}
+	}
+
+	return &ConcurrentSliceCache[E, T]{
+		pools: pools,
+	}
 }
 
 func (c *ConcurrentSliceCache[E, T]) Put(s T) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.cache.Put(s)
+	log2 := 64 - bits.LeadingZeros64(uint64(cap(s))-1)
+	if log2 >= len(poolSizes) {
+		return
+	}
+	c.pools[log2].Put(s[:0])
 }
 
 func (c *ConcurrentSliceCache[E, T]) Get(minCap int) T {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.cache.Get(minCap)
-}
+	if minCap < 0 {
+		panic("invalid capacity")
+	}
+	if minCap == 0 {
+		return T{}
+	}
 
-func (c *ConcurrentSliceCache[E, T]) Clear() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.cache.Clear()
+	// log₂ of next power of two
+	log2 := 64 - bits.LeadingZeros64(uint64(minCap)-1)
+	if log2 >= len(poolSizes) {
+		// This is larger than any slices we cache
+		return make(T, 0, minCap)
+	}
+
+	return c.pools[log2].Get().(T)[:0]
 }
