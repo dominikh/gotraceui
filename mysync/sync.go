9fb59798ef93e45ef35b98ea7b5b2b6e02e33d5b
Dominik Honnef <dominik@honnef.co>
2023-12-14 23:01:22 +0100

    mysync: add Mutex type that wraps the protected value

--- mysync/sync.go
+++ mysync/sync.go
@@ -45,3 +45,33 @@ func Distribute[T any](items []T, limit int, fn func(group int, step int, subite
 	wg.Wait()
 	return gerr
 }
+
+type Mutex[T any] struct {
+	mu sync.RWMutex
+	v  T
+}
+
+type MutexUnlock struct {
+	mu *sync.RWMutex
+}
+
+type MutexRUnlock struct {
+	mu *sync.RWMutex
+}
+
+func NewMutex[T any](v T) *Mutex[T] {
+	return &Mutex[T]{v: v}
+}
+
+func (mu *Mutex[T]) Lock() (T, MutexUnlock) {
+	mu.mu.Lock()
+	return mu.v, MutexUnlock{&mu.mu}
+}
+
+func (mu *Mutex[T]) RLock() (T, MutexRUnlock) {
+	mu.mu.RLock()
+	return mu.v, MutexRUnlock{&mu.mu}
+}
+
+func (u MutexUnlock) Unlock()   { u.mu.Unlock() }
+func (u MutexRUnlock) RUnlock() { u.mu.RUnlock() }
