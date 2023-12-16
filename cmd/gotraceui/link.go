82e047167e2ed3a86e497f010200beb67d7552ae
Dominik Honnef <dominik@honnef.co>
2023-12-16 01:44:08 +0100

    Fix composite animations
    We can't first initiate a scroll to a timeline, then animate to a time range using
    the current Y coordinate and expect the two to combine.

--- cmd/gotraceui/link.go
+++ cmd/gotraceui/link.go
@@ -543,17 +543,17 @@ func (l *OpenSpansAction) Open(gtx layout.Context, mwin *MainWindow) {
 func (l *ScrollAndPanToSpansAction) Open(gtx layout.Context, mwin *MainWindow) {
 	c, ok := l.Spans.Container()
 	assert(ok, "expected container")
-	mwin.canvas.scrollToTimeline(gtx, c.Timeline)
+	y := mwin.canvas.timelineY(gtx, c.Timeline)
 	d := mwin.canvas.End() - mwin.canvas.start
 	ts := l.Spans.At(0).Start + trace.Timestamp(SpansDuration(l.Spans)/2)
-	mwin.canvas.navigateTo(gtx, ts-d/2, mwin.canvas.nsPerPx, mwin.canvas.y)
+	mwin.canvas.navigateTo(gtx, ts-d/2, mwin.canvas.nsPerPx, y)
 }
 
 func (l *ZoomToSpansAction) Open(gtx layout.Context, mwin *MainWindow) {
 	c, ok := l.Spans.Container()
 	assert(ok, "expected container")
-	mwin.canvas.scrollToTimeline(gtx, c.Timeline)
-	mwin.canvas.navigateToStartAndEnd(gtx, l.Spans.At(0).Start, LastSpan(l.Spans).End, mwin.canvas.y)
+	y := mwin.canvas.timelineY(gtx, c.Timeline)
+	mwin.canvas.navigateToStartAndEnd(gtx, l.Spans.At(0).Start, LastSpan(l.Spans).End, y)
 }
 
 func handleLinkClick(win *theme.Window, ev gesture.ClickEvent, link ObjectLink) {
