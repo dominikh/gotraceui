dominikh/gotraceui

commit 5efceb44e4ecb9b00612ebf25da2faf4df69eea9

Backport "gesture: [API] rename gesture state update methods to Update"
commit c756986d9e79517bdb1efaf5f1fcd636ebb73c00
Author: Elias Naur <mail@eliasnaur.com>
Date:   Sat Oct 7 10:30:34 2023 -0500

    gesture: [API] rename gesture state update methods to Update

    Change the gesture state update methods to align with the convention.

    Signed-off-by: Elias Naur <mail@eliasnaur.com>

cmd/gotraceui/canvas.go
@@ -615,10 +615,7 @@ func (cv *Canvas) Layout(win *theme.Window, gtx layout.Context) layout.Dimension
 		cv.nsPerPx = (float64(end) + 2*slack) / float64(cv.width)
 	}
 
-	cv.timeline.hover.Update(gtx.Queue)
-	cv.hover.Update(gtx.Queue)
-
-	if cv.hover.Hovered() {
+	if cv.hover.Update(gtx.Queue) {
 		cv.pointerAt = cv.hover.Pointer()
 	}
 
@@ -1046,7 +1043,7 @@ func (cv *Canvas) Layout(win *theme.Window, gtx layout.Context) layout.Dimension
 		if clicked := tl.widget.ClickedSpans(); clicked.Len() > 0 {
 			cv.clickedSpans = append(cv.clickedSpans, clicked)
 		}
-		if tl.widget.Hovered() {
+		if tl.widget.Hovered(gtx) {
 			cv.timeline.hoveredTimeline = tl
 			spans := tl.widget.HoveredSpans()
 			if spans.Len() > 0 {
@@ -1097,7 +1094,7 @@ func (cv *Canvas) layoutTimelines(win *theme.Window, gtx layout.Context) (layout
 	for i := start; i < end; i++ {
 		tl := cv.timelines[i]
 		stack := op.Offset(image.Pt(0, y)).Push(gtx.Ops)
-		topBorder := i > 0 && cv.timelines[i-1].widget.Hovered()
+		topBorder := i > 0 && cv.timelines[i-1].widget.Hovered(gtx)
 		tl.Layout(win, gtx, cv, cv.timeline.displayAllLabels, cv.timeline.compact, topBorder, &cv.trackSpanLabels)
 		stack.Pop()
 
@@ -1177,7 +1174,7 @@ func (axis *Axis) Layout(win *theme.Window, gtx layout.Context) (dims layout.Dim
 	axis.click.Add(gtx.Ops)
 	axis.drag.Add(gtx.Ops)
 
-	for _, ev := range axis.click.Events(gtx.Queue) {
+	for _, ev := range axis.click.Update(gtx.Queue) {
 		if ev.Kind == gesture.KindPress && ev.Button == pointer.ButtonSecondary {
 			win.SetContextMenu(
 				[]*theme.MenuItem{

cmd/gotraceui/link.go
@@ -571,7 +571,7 @@ func handleLinkClick(win *theme.Window, ev gesture.ClickEvent, link ObjectLink) 
 func handleLinkClicks(win *theme.Window, gtx layout.Context, clicks *mem.BucketSlice[Link]) {
 	for i := 0; i < clicks.Len(); i++ {
 		link := clicks.Ptr(i)
-		for _, ev := range link.Events(gtx.Queue) {
+		for _, ev := range link.Update(gtx.Queue) {
 			handleLinkClick(win, ev, link.Link)
 		}
 	}

cmd/gotraceui/plot.go
@@ -134,7 +134,7 @@ func (pl *Plot) Layout(win *theme.Window, gtx layout.Context, cv *Canvas) layout
 	pl.hover.Add(gtx.Ops)
 
 	var clicked bool
-	for _, click := range pl.click.Events(gtx.Queue) {
+	for _, click := range pl.click.Update(gtx.Queue) {
 		if click.Kind == gesture.KindPress && click.Button == pointer.ButtonSecondary {
 			clicked = true
 			break

cmd/gotraceui/text.go
@@ -143,7 +143,7 @@ func (txt *Text) Layout(win *theme.Window, gtx layout.Context, spans []TextSpan)
 	for i := range spans {
 		s := &spans[i]
 		if s.Click != nil {
-			for _, ev := range s.Click.Events(gtx.Queue) {
+			for _, ev := range s.Click.Update(gtx.Queue) {
 				txt.events = append(txt.events, TextEvent{s, ev})
 			}
 			if s.Click.Hovered() {

cmd/gotraceui/timeline.go
@@ -84,11 +84,11 @@ type TimelineWidget struct {
 	usedSuboptimalTexture time.Time
 }
 
-func (tw *TimelineWidget) Hovered() bool {
+func (tw *TimelineWidget) Hovered(gtx layout.Context) bool {
 	if tw == nil {
 		return false
 	}
-	return tw.hover.Hovered()
+	return tw.hover.Update(gtx.Queue)
 }
 
 type SpanTooltipState struct {
@@ -462,12 +462,12 @@ func (tl *Timeline) Layout(
 	tl.widget.hover.Add(gtx.Ops)
 
 	if !compact {
-		if tl.widget.Hovered() || forceLabel || topBorder {
+		if tl.widget.Hovered(gtx) || forceLabel || topBorder {
 			// Draw border at top of the timeline
 			theme.FillShape(win, gtx.Ops, colors[colorTimelineBorder], clip.Rect{Max: image.Pt(gtx.Constraints.Max.X, gtx.Dp(1))}.Op())
 		}
 
-		if tl.widget.Hovered() || forceLabel {
+		if tl.widget.Hovered(gtx) || forceLabel {
 			tl.widget.labelClick.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 				labelGtx := gtx
 				labelGtx.Constraints.Min = image.Point{}
@@ -719,7 +719,7 @@ func (track *Track) Layout(
 
 	// We're passing gtx.Queue instead of gtx to avoid allocations because of convT. This means gtx.Queue mustn't be
 	// nil.
-	for _, ev := range track.widget.click.Events(gtx.Queue) {
+	for _, ev := range track.widget.click.Update(gtx.Queue) {
 		if ev.Kind == gesture.KindClick && ev.Button == pointer.ButtonPrimary {
 			switch ev.Modifiers {
 			case key.ModShortcut:
@@ -751,7 +751,7 @@ func (track *Track) Layout(
 	}
 
 	// // OPT(dh): don't redraw if the only change is cv.y
-	if !track.widget.hover.Hovered() &&
+	if !track.widget.hover.Update(gtx.Queue) &&
 		!track.widget.prevFrame.hovered &&
 		cv.unchanged(gtx) &&
 		(tl.invalidateCache == nil || !tl.invalidateCache(tl, cv)) &&
@@ -764,7 +764,7 @@ func (track *Track) Layout(
 		return track.widget.prevFrame.dims
 	}
 
-	track.widget.prevFrame.hovered = track.widget.hover.Hovered()
+	track.widget.prevFrame.hovered = track.widget.hover.Update(gtx.Queue)
 	track.widget.prevFrame.constraints = gtx.Constraints
 
 	origOps := gtx.Ops
@@ -806,7 +806,7 @@ func (track *Track) Layout(
 			return
 		}
 		hovered := false
-		if track.widget.hover.Hovered() && track.widget.hover.Pointer().X >= startPx && track.widget.hover.Pointer().X < endPx && haveSpans {
+		if track.widget.hover.Update(gtx.Queue) && track.widget.hover.Pointer().X >= startPx && track.widget.hover.Pointer().X < endPx && haveSpans {
 			// Highlight the span under the cursor
 			hovered = true
 			track.widget.hoveredSpans = dspSpans
@@ -945,7 +945,7 @@ func (track *Track) Layout(
 				eventsPath.LineTo(f32.Pt(minX, maxY))
 				eventsPath.Close()
 
-				if cv.timeline.showTooltips < showTooltipsNone && track.widget.hover.Hovered() && track.widget.hover.Pointer().X >= minX && track.widget.hover.Pointer().X < maxX {
+				if cv.timeline.showTooltips < showTooltipsNone && track.widget.hover.Update(gtx.Queue) && track.widget.hover.Pointer().X >= minX && track.widget.hover.Pointer().X < maxX {
 					spanTooltipState.eventsUnderCursor = events.Slice(oldi, i+1)
 				}
 			}

gesture/gesture.go
@@ -93,8 +93,8 @@ func (c *Click) Pressed(btn pointer.Buttons) bool {
 	return false
 }
 
-// Events returns the next click events, if any.
-func (c *Click) Events(q event.Queue) []ClickEvent {
+// Update state and return the click events.
+func (c *Click) Update(q event.Queue) []ClickEvent {
 	var events []ClickEvent
 	for _, evt := range q.Events(c) {
 		e, ok := evt.(pointer.Event)
@@ -201,11 +201,6 @@ type Hover struct {
 	pointerAt f32.Point
 }
 
-// Hovered returns whether a pointer is inside the area.
-func (h *Hover) Hovered() bool {
-	return h.hovered
-}
-
 // Add the handler to the operation list to detect hovering.
 func (h *Hover) Add(ops *op.Ops) {
 	defer pointer.PassOp{}.Push(ops).Pop()
@@ -215,7 +210,7 @@ func (h *Hover) Add(ops *op.Ops) {
 	}.Add(ops)
 }
 
-func (h *Hover) Update(q event.Queue) {
+func (h *Hover) Update(q event.Queue) bool {
 	for _, evt := range q.Events(h) {
 		e, ok := evt.(pointer.Event)
 		if !ok {
@@ -235,6 +230,7 @@ func (h *Hover) Update(q event.Queue) {
 			h.hovered = true
 		}
 	}
+	return h.hovered
 }
 
 func (h *Hover) Pointer() f32.Point {

theme/cmdpalette.go
@@ -323,7 +323,7 @@ func (pl *CommandPalette) Layout(win *Window, gtx layout.Context) layout.Dimensi
 										pl.active = index
 									}
 								}
-								for _, ev := range ges.Events(gtx.Queue) {
+								for _, ev := range ges.Update(gtx.Queue) {
 									if ev.Kind == gesture.KindClick {
 										win.EmitAction(pl.cmds.At(pl.filtered[index]).Link())
 										win.CloseModal()

theme/flamegraph.go
@@ -149,7 +149,7 @@ func (fg FlameGraphStyle) Layout(win *Window, gtx layout.Context) (dims layout.D
 		levelHeight = float32(height + gtx.Dp(rowSpacingDp))
 	)
 
-	if fg.StyleState.hover.Hovered() {
+	if fg.StyleState.hover.Update(gtx.Queue) {
 		ptPx = fg.StyleState.hover.Pointer()
 	}
 
@@ -166,7 +166,7 @@ func (fg FlameGraphStyle) Layout(win *Window, gtx layout.Context) (dims layout.D
 	fg.StyleState.hover.Update(gtx.Queue)
 
 	var trackClicked bool
-	for _, ev := range fg.StyleState.click.Events(gtx.Queue) {
+	for _, ev := range fg.StyleState.click.Update(gtx.Queue) {
 		if ev.Kind == gesture.KindClick &&
 			ev.Button == pointer.ButtonPrimary &&
 			ev.Modifiers == key.ModShortcut {

commit a47d112a77b66dd836dd66d596a6f4c610e4236e

Fix tracking of timeline-relative pointer position
This broke keeping the timeline under the cursor when toggling stack
tracks.

Fixes a regression introduced in
5efceb44e4ecb9b00612ebf25da2faf4df69eea9.

cmd/gotraceui/canvas.go
@@ -626,6 +626,8 @@ func (cv *Canvas) Layout(win *theme.Window, gtx layout.Context) layout.Dimension
 		cv.nsPerPx = (float64(end) + 2*slack) / float64(cv.width)
 	}
 
+	cv.timeline.hover.Update(gtx.Queue)
+
 	if cv.hover.Update(gtx.Queue) {
 		cv.pointerAt = cv.hover.Pointer()
 	}