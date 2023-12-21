dominikh/gotraceui

commit b9535736fc5f585a70495328ee658c0af2dea432

Update most widgets for new split layout/debug paradigm

cmd/gotraceui/goroutine.go
@@ -980,6 +980,10 @@ func (evs *GoroutineList) HoveredLink() ObjectLink {
 
 func (gl *GoroutineList) SetGoroutines(win *theme.Window, gtx layout.Context, gs []*ptrace.Goroutine) {
 	gl.initTable(win, gtx)
+	gl.setGoroutines(gtx, gs)
+}
+
+func (gl *GoroutineList) setGoroutines(gtx layout.Context, gs []*ptrace.Goroutine) {
 	gl.Goroutines.Reset(gs)
 
 	switch gl.table.Columns[gl.table.SortedBy].Name {
@@ -1108,10 +1112,18 @@ func (gs *GoroutineList) initTable(win *theme.Window, gtx layout.Context) {
 	gs.table.Columns[1].Width = max(0, gs.table.Columns[1].Width-float32(d))
 }
 
+func (gs *GoroutineList) Update(gtx layout.Context) {
+	if _, ok := gs.table.SortByClickedColumn(); ok {
+		// Trigger resorting.
+		gs.setGoroutines(gtx, gs.Goroutines.Items)
+	}
+}
+
 func (gs *GoroutineList) Layout(win *theme.Window, gtx layout.Context) layout.Dimensions {
 	defer rtrace.StartRegion(context.Background(), "main.GoroutineList.Layout").End()
 
 	gs.initTable(win, gtx)
+	gs.Update(gtx)
 	gs.cellFormatter.Update(win, gtx)
 
 	cellFn := func(win *theme.Window, gtx layout.Context, row, col int) layout.Dimensions {
@@ -1178,11 +1190,6 @@ func (gs *GoroutineList) Layout(win *theme.Window, gtx layout.Context) layout.Di
 		gs.Goroutines.Len(),
 		cellFn,
 	)
-
-	if _, ok := gs.table.SortByClickedColumn(); ok {
-		// Trigger resorting.
-		gs.SetGoroutines(win, gtx, gs.Goroutines.Items)
-	}
 
 	return dims
 }

cmd/gotraceui/span.go
@@ -67,6 +67,7 @@ type SpansInfo struct {
 	descriptionBuilder func(win *theme.Window, gtx layout.Context) Description
 	descriptionText    Text
 	hoveredLink        ObjectLink
+	prevSpans          []TextSpan
 
 	stacktraceList widget.List
 
@@ -331,10 +332,10 @@ func (si *SpansInfo) Layout(win *theme.Window, gtx layout.Context) layout.Dimens
 		}
 	}
 
-	for _, ev := range si.descriptionText.Events() {
+	for _, ev := range si.descriptionText.Update(gtx, si.prevSpans) {
 		handleLinkClick(win, ev.Event, ev.Span.ObjectLink)
 	}
-	for _, ev := range si.eventList.Clicked() {
+	for _, ev := range si.eventList.Update(gtx) {
 		handleLinkClick(win, ev.Event, ev.Span.ObjectLink)
 	}
 
@@ -398,7 +399,7 @@ func (si *SpansInfo) Layout(win *theme.Window, gtx layout.Context) layout.Dimens
 		si.mwin.EmitAction(&OpenPanelAction{NewSpansInfo(cfg, si.trace, si.mwin, ft, si.allTimelines)})
 	}
 
-	if si.hist.Changed() {
+	if si.hist.Update(gtx) {
 		si.computeHistogram(win, &si.hist.Config)
 	}
 
@@ -470,7 +471,9 @@ func (si *SpansInfo) Layout(win *theme.Window, gtx layout.Context) layout.Dimens
 				gtx.Constraints.Min = image.Point{}
 				si.descriptionText.Reset(win.Theme)
 				desc := si.descriptionBuilder(win, gtx)
-				return desc.Layout(win, gtx, &si.descriptionText)
+				dims, spans := desc.Layout(win, gtx, &si.descriptionText)
+				si.prevSpans = spans
+				return dims
 			},
 			func(gtx layout.Context) layout.Dimensions {
 				if spans.Len() == 1 && spans.AtPtr(0).State == ptrace.StateUserRegion {

cmd/gotraceui/statistics.go
@@ -279,6 +279,10 @@ func (gs *SpansStats) Layout(win *theme.Window, gtx layout.Context) layout.Dimen
 		gs.table.SortedBy = 0
 	}
 
+	if _, ok := gs.table.SortByClickedColumn(); ok {
+		gs.sort()
+	}
+
 	cellFn := func(win *theme.Window, gtx layout.Context, row, col int) layout.Dimensions {
 		var value, unit string
 		switch col {
@@ -318,10 +322,6 @@ func (gs *SpansStats) Layout(win *theme.Window, gtx layout.Context) layout.Dimen
 	}
 
 	dims := theme.SimpleTable(win, gtx, &gs.table, &gs.scrollState, gs.stats.Len(), cellFn)
-
-	if _, ok := gs.table.SortByClickedColumn(); ok {
-		gs.sort()
-	}
 
 	return dims
 }

cmd/gotraceui/events.go
@@ -35,6 +35,7 @@ type EventList struct {
 
 	timestampObjects mem.BucketSlice[trace.Timestamp]
 	texts            mem.BucketSlice[Text]
+	prevSpans        []TextSpan
 }
 
 func (evs *EventList) UpdateFilter() {
@@ -82,17 +83,6 @@ func (evs *EventList) UpdateFilter() {
 			max = n
 		}
 	}
-}
-
-// Clicked returns all objects of text spans that have been clicked during the last call to Layout.
-func (evs *EventList) Clicked() []TextEvent {
-	// This only allocates when links have been clicked, which is a very low frequency event.
-	var out []TextEvent
-	for i := 0; i < evs.texts.Len(); i++ {
-		txt := evs.texts.Ptr(i)
-		out = append(out, txt.Events()...)
-	}
-	return out
 }
 
 // HoveredLink returns the link that has been hovered during the last call to Layout.
@@ -181,6 +171,27 @@ func (evs *EventList) sort() {
 	})
 }
 
+func (evs *EventList) Update(gtx layout.Context) []TextEvent {
+	evs.table.Update(gtx)
+
+	if _, ok := evs.table.SortByClickedColumn(); ok {
+		evs.sort()
+	}
+
+	if evs.Filter.ShowGoCreate.Update(gtx) ||
+		evs.Filter.ShowGoUnblock.Update(gtx) ||
+		evs.Filter.ShowGoSysCall.Update(gtx) ||
+		evs.Filter.ShowUserLog.Update(gtx) {
+		evs.UpdateFilter()
+	}
+
+	for i := 0; i < evs.texts.Len(); i++ {
+		evs.texts.Ptr(i).Update(gtx, evs.prevSpans)
+	}
+
+	return nil
+}
+
 func (evs *EventList) Layout(win *theme.Window, gtx layout.Context) layout.Dimensions {
 	defer rtrace.StartRegion(context.Background(), "main.EventList.Layout").End()
 
@@ -192,18 +203,10 @@ func (evs *EventList) Layout(win *theme.Window, gtx layout.Context) layout.Dimen
 		evs.table.SetColumns(win, gtx, cols)
 	}
 
-	if _, ok := evs.table.SortByClickedColumn(); ok {
-		evs.sort()
-	}
+	evs.Update(gtx)
 
 	evs.timestampObjects.Reset()
-
-	if evs.Filter.ShowGoCreate.Update(gtx) ||
-		evs.Filter.ShowGoUnblock.Update(gtx) ||
-		evs.Filter.ShowGoSysCall.Update(gtx) ||
-		evs.Filter.ShowUserLog.Update(gtx) {
-		evs.UpdateFilter()
-	}
+	evs.prevSpans = evs.prevSpans[:0]
 
 	var txtCnt int
 	cellFn := func(win *theme.Window, gtx layout.Context, row, col int) layout.Dimensions {
@@ -276,6 +279,7 @@ func (evs *EventList) Layout(win *theme.Window, gtx layout.Context) layout.Dimen
 
 		dims := txt.Layout(win, gtx, tb.Spans)
 		dims.Size = gtx.Constraints.Constrain(dims.Size)
+		evs.prevSpans = append(evs.prevSpans, tb.Spans...)
 		return dims
 	}
 

cmd/gotraceui/function.go
@@ -31,6 +31,7 @@ type FunctionInfo struct {
 
 	descriptionText Text
 	hoveredLink     ObjectLink
+	prevSpans       []TextSpan
 
 	initialized bool
 
@@ -130,6 +131,30 @@ func (fi *FunctionInfo) Layout(win *theme.Window, gtx layout.Context) layout.Dim
 		fi.initialized = true
 	}
 
+	for _, ev := range fi.descriptionText.Update(gtx, fi.prevSpans) {
+		handleLinkClick(win, ev.Event, ev.Span.ObjectLink)
+	}
+	firstNonNil := func(els ...ObjectLink) ObjectLink {
+		for _, el := range els {
+			if el != nil {
+				return el
+			}
+		}
+		return nil
+	}
+	fi.hoveredLink = firstNonNil(
+		fi.goroutineList.HoveredLink(),
+		fi.descriptionText.HoveredLink(),
+	)
+
+	for fi.ComponentButtons.Backed(gtx) {
+		fi.mwin.EmitAction(&PrevPanelAction{})
+	}
+
+	if fi.hist.Update(gtx) {
+		fi.histGoroutines = fi.computeHistogram(win, &fi.hist.Config)
+	}
+
 	// Inset of 5 pixels on all sides. We can't use layout.Inset because it doesn't decrease the minimum constraint,
 	// which we do care about here.
 	gtx.Constraints.Min = gtx.Constraints.Min.Sub(image.Pt(2*5, 2*5))
@@ -155,7 +180,9 @@ func (fi *FunctionInfo) Layout(win *theme.Window, gtx layout.Context) layout.Dim
 		func(gtx layout.Context) layout.Dimensions {
 			gtx.Constraints.Min = image.Point{}
 			fi.descriptionText.Reset(win.Theme)
-			return fi.buildDescription(win, gtx).Layout(win, gtx, &fi.descriptionText)
+			dims, spans := fi.buildDescription(win, gtx).Layout(win, gtx, &fi.descriptionText)
+			fi.prevSpans = spans
+			return dims
 		},
 
 		func(gtx layout.Context) layout.Dimensions { return layout.Spacer{Height: 10}.Layout(gtx) },
@@ -192,30 +219,6 @@ func (fi *FunctionInfo) Layout(win *theme.Window, gtx layout.Context) layout.Dim
 			})
 		},
 	)
-
-	for _, ev := range fi.descriptionText.Events() {
-		handleLinkClick(win, ev.Event, ev.Span.ObjectLink)
-	}
-	firstNonNil := func(els ...ObjectLink) ObjectLink {
-		for _, el := range els {
-			if el != nil {
-				return el
-			}
-		}
-		return nil
-	}
-	fi.hoveredLink = firstNonNil(
-		fi.goroutineList.HoveredLink(),
-		fi.descriptionText.HoveredLink(),
-	)
-
-	for fi.ComponentButtons.Backed(gtx) {
-		fi.mwin.EmitAction(&PrevPanelAction{})
-	}
-
-	if fi.hist.Changed() {
-		fi.histGoroutines = fi.computeHistogram(win, &fi.hist.Config)
-	}
 
 	return dims
 }

cmd/gotraceui/histogram.go
@@ -25,6 +25,8 @@ type InteractiveHistogram struct {
 	settingsState HistogramSettingsState
 	click         widget.Clickable
 	changed       bool
+
+	shouldCloseModal bool
 }
 
 func (hist *InteractiveHistogram) Set(win *theme.Window, data []time.Duration) {
@@ -36,81 +38,91 @@ func (hist *InteractiveHistogram) Set(win *theme.Window, data []time.Duration) {
 	})
 }
 
-func (hist *InteractiveHistogram) Changed() bool {
-	b := hist.changed
-	hist.changed = false
-	return b
+type InteractiveHistogramUpdateResult struct {
+}
+
+func (hist *InteractiveHistogram) Update(gtx layout.Context) (changed bool) {
+	saved, cancelled := hist.settingsState.Update(gtx)
+	if saved {
+		hist.Config.Bins = hist.settingsState.NumBins()
+		hist.Config.RejectOutliers = hist.settingsState.RejectOutliers()
+		changed = true
+		hist.shouldCloseModal = true
+	}
+	if cancelled {
+		hist.settingsState.Reset(hist.Config)
+		hist.shouldCloseModal = true
+	}
+	if start, end, ok := hist.state.Update(gtx); ok {
+		hist.Config.Start = start
+		hist.Config.End = end
+		changed = true
+	}
+	if hist.changed {
+		hist.changed = false
+		changed = true
+	}
+	return changed
 }
 
 func (hist *InteractiveHistogram) Layout(win *theme.Window, gtx layout.Context) layout.Dimensions {
 	defer rtrace.StartRegion(context.Background(), "main.InteractiveHistogram.Layout").End()
 
+	hist.Update(gtx)
+
+	if hist.shouldCloseModal {
+		hist.shouldCloseModal = false
+		win.CloseModal()
+	}
+
+	for {
+		click, ok := hist.click.Clicked(gtx)
+		if !ok {
+			break
+		}
+		if click.Button != pointer.ButtonSecondary {
+			continue
+		}
+
+		menu := []*theme.MenuItem{
+			{
+				Label: PlainLabel("Change settings"),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						win.SetModal(func(win *theme.Window, gtx layout.Context) layout.Dimensions {
+							gtx.Constraints.Min = gtx.Constraints.Constrain(image.Pt(1000, 500))
+							gtx.Constraints.Max = gtx.Constraints.Min
+							return theme.Dialog(win.Theme, "Histogram settings").Layout(win, gtx, HistogramSettings(&hist.settingsState).Layout)
+						})
+					})
+				},
+			},
+
+			{
+				// TODO disable when there is nothing to zoom out to
+				Label: PlainLabel("Zoom out"),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						hist.Config.Start = 0
+						hist.Config.End = 0
+						hist.changed = true
+					})
+				},
+			},
+		}
+		win.SetContextMenu(menu)
+	}
+
 	whist, ok := hist.widget.Result()
 	if ok {
+		hist.state.Histogram = whist
 		thist := theme.Histogram(win.Theme, &hist.state)
 		thist.XLabel = "Duration"
 		thist.YLabel = "Count"
+
 		dims := hist.click.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-			return thist.Layout(win, gtx, whist)
+			return thist.Layout(win, gtx)
 		})
-
-		if hist.state.Activated != struct {
-			Start widget.FloatDuration
-			End   widget.FloatDuration
-		}{} {
-			hist.Config.Start = hist.state.Activated.Start
-			hist.Config.End = hist.state.Activated.End
-			hist.changed = true
-		}
-
-		for {
-			click, ok := hist.click.Clicked(gtx)
-			if !ok {
-				break
-			}
-			if click.Button != pointer.ButtonSecondary {
-				continue
-			}
-
-			menu := []*theme.MenuItem{
-				{
-					Label: PlainLabel("Change settings"),
-					Action: func() theme.Action {
-						return theme.ExecuteAction(func(gtx layout.Context) {
-							win.SetModal(func(win *theme.Window, gtx layout.Context) layout.Dimensions {
-								gtx.Constraints.Min = gtx.Constraints.Constrain(image.Pt(1000, 500))
-								gtx.Constraints.Max = gtx.Constraints.Min
-								return theme.Dialog(win.Theme, "Histogram settings").Layout(win, gtx, HistogramSettings(&hist.settingsState).Layout)
-							})
-						})
-					},
-				},
-
-				{
-					// TODO disable when there is nothing to zoom out to
-					Label: PlainLabel("Zoom out"),
-					Action: func() theme.Action {
-						return theme.ExecuteAction(func(gtx layout.Context) {
-							hist.Config.Start = 0
-							hist.Config.End = 0
-							hist.changed = true
-						})
-					},
-				},
-			}
-			win.SetContextMenu(menu)
-		}
-
-		if hist.settingsState.Saved() {
-			hist.Config.Bins = hist.settingsState.NumBins()
-			hist.Config.RejectOutliers = hist.settingsState.RejectOutliers()
-			hist.changed = true
-			win.CloseModal()
-		}
-		if hist.settingsState.Cancelled() {
-			hist.settingsState.Reset(hist.Config)
-			win.CloseModal()
-		}
 
 		return dims
 	} else {
@@ -119,24 +131,21 @@ func (hist *InteractiveHistogram) Layout(win *theme.Window, gtx layout.Context) 
 }
 
 type HistogramSettingsState struct {
-	saved, cancelled bool
-
 	numBinsEditor  widget.Editor
 	filterOutliers widget.Bool
 	save           widget.PrimaryClickable
 	cancel         widget.PrimaryClickable
 }
 
-func (hss *HistogramSettingsState) Saved() bool {
-	b := hss.saved
-	hss.saved = false
-	return b
-}
+func (hs *HistogramSettingsState) Update(gtx layout.Context) (saved, cancelled bool) {
+	for hs.save.Clicked(gtx) {
+		saved = true
+	}
+	for hs.cancel.Clicked(gtx) {
+		cancelled = true
+	}
 
-func (hss *HistogramSettingsState) Cancelled() bool {
-	b := hss.cancelled
-	hss.cancelled = false
-	return b
+	return saved, cancelled
 }
 
 func (hss *HistogramSettingsState) NumBins() int {
@@ -172,9 +181,6 @@ func HistogramSettings(state *HistogramSettingsState) HistogramSettingsStyle {
 
 func (hs HistogramSettingsStyle) Layout(win *theme.Window, gtx layout.Context) layout.Dimensions {
 	defer rtrace.StartRegion(context.Background(), "main.HistogramSettingsStyle.Layout").End()
-
-	hs.State.saved = false
-	hs.State.cancelled = false
 
 	validateNumBins := func(s string) bool {
 		return len(s) <= 4
@@ -243,13 +249,6 @@ func (hs HistogramSettingsStyle) Layout(win *theme.Window, gtx layout.Context) l
 			)
 		},
 	)
-
-	for hs.State.save.Clicked(gtx) {
-		hs.State.saved = true
-	}
-	for hs.State.cancel.Clicked(gtx) {
-		hs.State.cancelled = true
-	}
 
 	return dims
 }

cmd/gotraceui/main.go
@@ -1639,7 +1639,7 @@ type DescriptionAttribute struct {
 	Value TextSpan
 }
 
-func (desc Description) Layout(win *theme.Window, gtx layout.Context, txt *Text) layout.Dimensions {
+func (desc Description) Layout(win *theme.Window, gtx layout.Context, txt *Text) (layout.Dimensions, []TextSpan) {
 	txt.Reset(win.Theme)
 	// OPT(dh): reuse space
 	tb := TextBuilder{Window: win}
@@ -1649,7 +1649,7 @@ func (desc Description) Layout(win *theme.Window, gtx layout.Context, txt *Text)
 		tb.Span("\n")
 	}
 
-	return txt.Layout(win, gtx, tb.Spans)
+	return txt.Layout(win, gtx, tb.Spans), tb.Spans
 }
 
 type ScrollToTimelineCommand struct {

cmd/gotraceui/plot.go
@@ -141,6 +141,74 @@ func (pl *Plot) Layout(win *theme.Window, gtx layout.Context, cv *Canvas) layout
 		}
 	}
 
+	if clicked {
+		r := rtrace.StartRegion(context.Background(), "context menu")
+		items := []*theme.MenuItem{
+			{
+				Label: PlainLabel("Reset extents"),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						pl.min = 0
+						_, pl.max = pl.computeExtents(0, math.MaxInt64)
+						pl.autoScale = false
+					})
+				},
+			},
+			{
+				Label: PlainLabel("Set extents to global extrema"),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						pl.min, pl.max = pl.computeExtents(0, math.MaxInt64)
+					})
+				},
+			},
+			{
+				Label: PlainLabel("Set extents to local extrema"),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						pl.min, pl.max = pl.computeExtents(cv.start, cv.End())
+					})
+				},
+			},
+			{
+				Label: ToggleLabel("Don't auto-set extents", "Auto-set extents to local extrema", &pl.autoScale),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						pl.autoScale = !pl.autoScale
+					})
+				},
+			},
+			{
+				Label: ToggleLabel("Show legends", "Hide legends", &pl.hideLegends),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						pl.hideLegends = !pl.hideLegends
+					})
+				},
+			},
+		}
+		for i := range pl.series {
+			s := &pl.series[i]
+			var label string
+			if s.disabled {
+				label = fmt.Sprintf("Show %q series", s.Name)
+			} else {
+				label = fmt.Sprintf("Hide %q series", s.Name)
+			}
+			item := &theme.MenuItem{
+				Label: PlainLabel(label),
+				Action: func() theme.Action {
+					return theme.ExecuteAction(func(gtx layout.Context) {
+						s.disabled = !s.disabled
+					})
+				},
+			}
+			items = append(items, item)
+		}
+		win.SetContextMenu(items)
+		r.End()
+	}
+
 	var bitmap uint64
 	for i, s := range pl.series {
 		if s.disabled {
@@ -251,74 +319,6 @@ func (pl *Plot) Layout(win *theme.Window, gtx layout.Context, cv *Canvas) layout
 				return theme.Tooltip(win.Theme, strings.Join(lines, "\n")).Layout(win, gtx)
 			})
 		}
-		r.End()
-	}
-
-	if clicked {
-		r := rtrace.StartRegion(context.Background(), "context menu")
-		items := []*theme.MenuItem{
-			{
-				Label: PlainLabel("Reset extents"),
-				Action: func() theme.Action {
-					return theme.ExecuteAction(func(gtx layout.Context) {
-						pl.min = 0
-						_, pl.max = pl.computeExtents(0, math.MaxInt64)
-						pl.autoScale = false
-					})
-				},
-			},
-			{
-				Label: PlainLabel("Set extents to global extrema"),
-				Action: func() theme.Action {
-					return theme.ExecuteAction(func(gtx layout.Context) {
-						pl.min, pl.max = pl.computeExtents(0, math.MaxInt64)
-					})
-				},
-			},
-			{
-				Label: PlainLabel("Set extents to local extrema"),
-				Action: func() theme.Action {
-					return theme.ExecuteAction(func(gtx layout.Context) {
-						pl.min, pl.max = pl.computeExtents(cv.start, cv.End())
-					})
-				},
-			},
-			{
-				Label: ToggleLabel("Don't auto-set extents", "Auto-set extents to local extrema", &pl.autoScale),
-				Action: func() theme.Action {
-					return theme.ExecuteAction(func(gtx layout.Context) {
-						pl.autoScale = !pl.autoScale
-					})
-				},
-			},
-			{
-				Label: ToggleLabel("Show legends", "Hide legends", &pl.hideLegends),
-				Action: func() theme.Action {
-					return theme.ExecuteAction(func(gtx layout.Context) {
-						pl.hideLegends = !pl.hideLegends
-					})
-				},
-			},
-		}
-		for i := range pl.series {
-			s := &pl.series[i]
-			var label string
-			if s.disabled {
-				label = fmt.Sprintf("Show %q series", s.Name)
-			} else {
-				label = fmt.Sprintf("Hide %q series", s.Name)
-			}
-			item := &theme.MenuItem{
-				Label: PlainLabel(label),
-				Action: func() theme.Action {
-					return theme.ExecuteAction(func(gtx layout.Context) {
-						s.disabled = !s.disabled
-					})
-				},
-			}
-			items = append(items, item)
-		}
-		win.SetContextMenu(items)
 		r.End()
 	}
 

commit 1899855098954a12220bffe13e0a9cd67acfd8fc

Make sure all tables can be sorted
Fixes a regression introduced in
b9535736fc5f585a70495328ee658c0af2dea432.

cmd/gotraceui/goroutine.go
@@ -1104,6 +1104,7 @@ func (gs *GoroutineList) initTable(win *theme.Window, gtx layout.Context) {
 }
 
 func (gs *GoroutineList) Update(gtx layout.Context) {
+	gs.table.Update(gtx)
 	if _, ok := gs.table.SortByClickedColumn(); ok {
 		// Trigger resorting.
 		gs.setGoroutines(gtx, gs.Goroutines.Items)

cmd/gotraceui/span.go
@@ -645,6 +645,7 @@ func (spans *SpanList) Layout(win *theme.Window, gtx layout.Context) layout.Dime
 		spans.table.SortOrder = theme.SortAscending
 	}
 
+	spans.table.Update(gtx)
 	if _, ok := spans.table.SortByClickedColumn(); ok {
 		spans.sort()
 	}

cmd/gotraceui/statistics.go
@@ -279,6 +279,7 @@ func (gs *SpansStats) Layout(win *theme.Window, gtx layout.Context) layout.Dimen
 		gs.table.SortedBy = 0
 	}
 
+	gs.table.Update(gtx)
 	if _, ok := gs.table.SortByClickedColumn(); ok {
 		gs.sort()
 	}