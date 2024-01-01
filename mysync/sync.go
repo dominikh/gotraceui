3b106cca03e775895d1dea22b3848cbfd637e447
Dominik Honnef <dominik@honnef.co>
2024-01-01 21:05:15 +0100

    mysync: add type-safe wrapper for sync.Pool

--- mysync/sync.go
+++ mysync/sync.go
@@ -99,3 +99,25 @@ func (mu *Mutex[T]) RLock() (T, MutexRUnlock) {
 
 func (u MutexUnlock) Unlock()   { u.mu.Unlock() }
 func (u MutexRUnlock) RUnlock() { u.mu.RUnlock() }
+
+type Pool[T any] struct {
+	pool sync.Pool
+}
+
+func NewPool[T any](fn func() T) *Pool[T] {
+	return &Pool[T]{
+		pool: sync.Pool{
+			New: func() any {
+				return fn()
+			},
+		},
+	}
+}
+
+func (p *Pool[T]) Put(v T) {
+	p.pool.Put(v)
+}
+
+func (p *Pool[T]) Get() T {
+	return p.pool.Get().(T)
+}
